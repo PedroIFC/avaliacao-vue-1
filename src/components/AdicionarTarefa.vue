3a6fc3dac411bfa75ec1cf9f50e0df4bea96aac9
PedroIFC <ps404412@gmail.com>
2024-08-30 11:48:47 -0300

    Avaliação Recuperação

---
+++ src/components/AdicionarTarefa.vue
@@ -0,0 +1,25 @@
+<script setup>
+const model = defineModel()
+defineEmits(['adicionar']);
+</script>
+
+<template>
+    <section>
+        <h2>Adicione uma nova tarefa</h2>
+        <input type="text" v-model="model" @keyup.enter="$emit('adicionar')" />
+        <button>Adicionar</button>
+    </section>
+</template>
+
+<style scoped>
+input {
+    width: 50%;
+    padding: 5px;
+    margin: 5px 0;
+}
+
+button {
+    padding: 5px;
+    margin: 5px 0;
+}
+</style>
